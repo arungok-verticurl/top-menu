49321d899aa1e5bbecfe6853252293642953b3cb
Arun Krishnasamy <arun.krishnasamy@verticurl.com>
2020-06-08 15:41:16 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 (function () {
-  const _ = {};
+  var _ = {};
   _.getEleClass = function (cls) {
-    const eles = document.getElementsByClassName(cls);
+    var eles = document.getElementsByClassName(cls);
     return eles.length > 1 ? eles : eles.length === 1 ? eles[0] : null;
   };
   _.getEleTag = function (tag) {
-    const eles = document.getElementsByTagName(tag);
+    var eles = document.getElementsByTagName(tag);
     return eles.length > 1 ? eles : eles.length === 1 ? eles[0] : null;
   };
   _.getComputedStyles = function (ele) {
@@ -25,22 +25,22 @@
     });
 
   _.init = function() {
-    const ang = 180 / _.data.length;
+    var ang = 180 / _.data.length;
     _.data.map(function(nav) {
-      const span = document.createElement("span");
-      const a = document.createElement("a");
+      var span = document.createElement("span");
+      var a = document.createElement("a");
       a.href = "javascript:void(0)";
       a.title = nav.name;
-      const i = document.createElement("i");
+      var i = document.createElement("i");
       i.className = nav.icon;
       a.append(i);
       span.append(a);
       _.navEle.append(span);
     });
-    for (const index in _.navEle.childNodes) {
-      const a = _.navEle.childNodes[index];
+    for (var index in _.navEle.childNodes) {
+      var a = _.navEle.childNodes[index];
       if (!isNaN(index)) {
-        const tmp = _.getComputedStyles(a);
+        var tmp = _.getComputedStyles(a);
         a.style.paddingTop = _.navEle.childNodes.length * 50 + "px";
         a.style.transform = `translateX(-50%) rotate(${
           -((index * 1 + 1) * (ang - 1.95)) + 90
